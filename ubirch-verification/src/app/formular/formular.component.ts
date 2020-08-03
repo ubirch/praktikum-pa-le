690678e55788c3ecd30a442c909e89450d2c6758
paul.lengert <paullengert0510@gmail.com>
2020-08-03 18:20:20 +0200

    form validation with feedback and JSON generation

--- ubirch-verification/src/app/formular/formular.component.ts
+++ ubirch-verification/src/app/formular/formular.component.ts
@@ -9,26 +9,45 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 export class FormularComponent implements OnInit {
 
   form: FormGroup;
-  birthDate: any;
+  formData: any;
 
   constructor(private formbuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.form = this.formbuilder.group({
-      fName: [null, Validators.required],
-      gName: [null, Validators.required],
-      birthDate: [null, Validators.required],
-      idNumber: [null, Validators.required],
-      labId: [null, Validators.required],
-      testDateTime: [null, Validators.required],
-      testType: [null, Validators.required],
-      testResult: [null, [Validators.required, Validators.pattern('x|n|p')]],
-      ranNum: [null, Validators.required]
+      fName: ['Mustermann', [Validators.required, Validators.pattern(/^[a-zA-Z\s]*$/)]],
+      gName: ['Erika', [Validators.required, Validators.pattern(/^[a-zA-Z\s]*$/)]],
+      birthDate: [19640812, Validators.required],
+      idNumber: ['T01000322', Validators.required],
+      labId: ['3CF75K8D0L', Validators.required],
+      testDateTime: [202007011030, Validators.required],
+      testType: ['PCR', Validators.required],
+      testResult: ['n', [Validators.required, Validators.pattern('x|n|p')]],
+      ranNum: ['2fe00c151cb726bb9ed7', Validators.required]
     });
   }
 
-  onClickVerify(): void{
-    console.log(this.birthDate);
-  }
+  get fName() { return this.form.get('fName'); }
+  get gName() { return this.form.get('gName'); }
+  get birthDate() { return this.form.get('birthDate'); }
+  get idNumber() { return this.form.get('idNumber'); }
+  get labId() { return this.form.get('labId'); }
+  get testDateTime() { return this.form.get('testDateTime'); }
+  get testType() { return this.form.get('testType'); }
+  get testResult() { return this.form.get('testResult'); }
+  get ranNum() { return this.form.get('ranNum'); }
 
+  createJson(){
+   this.formData = JSON.stringify({
+      b: this.birthDate.value,
+      d: this.testDateTime.value,
+      f: this.fName.value,
+      g: this.gName.value,
+      i: this.labId.value,
+      p: this.idNumber.value,
+      r: this.testResult.value,
+      s: this.ranNum.value,
+      t: this.testType.value,
+    });
+  }
 }
